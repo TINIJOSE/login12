b2cc94910150072d12e67d3855dba5d484221777
tinijose <tinijose92@gmail.com>
2024-05-01 15:01:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+const router=require("./router.js")
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
+app.use('/route',router);
+
+// Home
+app.get('/', (req, res) => {
+    res.render('base', { title: "login system" });
+});
+
+app.listen(port, () => {
+    console.log(`Listening to the server on http://localhost:${port}`);
+});
